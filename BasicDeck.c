fea404f66ffba6bfadec1b883eed5cd8e8330176
VolcanoBakemeat <thedarlingoflumberland@gmail.com>
2016-03-11 10:57:42 -0800

    Update BasicDeck.c
    moved randnum into a separate functions

--- BasicDeck.c
+++ BasicDeck.c
@@ -5,12 +5,35 @@
 #define MAXLEN 80
 
 //Here's an idea, put player handles in a seperate array. Location in handle array correspond to hands in play array. This works like a dreamfairy
-//Also, make player number changeable.
+//Also, make player number changeable. Scrapped for now.
+/*
+	randomly chooses card.
+*/
+int drawcard(void)
+{
+	int r;
+    r= rand()/1000;
+    return r;
+}
+/*
+	This function (right now hardcoded so it only works with playnum =4) displays hands for each player and allows player to select card to put into table array
+*/
+int playball (int player, int p[4][7])
+{
+	int k;
+	int l;
+	for(k=0;k<4;++k)
+	{
+		printf("Player %d cards:",k+1);
+	}
+}
+
+
 int main (void)
 {
 
   int deckmaster[52];
-
+  
   int i;      //gen deck counter
 
   int m=0;   //printdeck counter
@@ -20,12 +43,11 @@ int main (void)
   int r;
   short unsigned int handnum;
   srand(time(NULL));	//initializes rand
-  r= rand();
  
   printf("How many players?\n"); 
   scanf("%d",&handnum);
   getchar();
- 
+  int table[handnum]; //cards played on table
   int play[handnum][7];	//cards in play
  
  // char handles[handnum][MAXLEN];	Abandoning handles to work on something more productive
@@ -63,19 +85,19 @@ int main (void)
 	}
 */
 
-  for(x=0; x<handnum; ++x)     //Now fills array play instead of simply printing. Plan to randomise in future.
+	//fills player hands. Ooh, ooh, create a drawcard function
+  for(x=0; x<handnum; ++x)     //Now fills array play instead of simply printing.
 
   {
 	printf("Player %d:\n",x+1);
    for(y=0; y<7; ++y)
    {
-   	play[x][y]= deckmaster[rand()/1000];
-   	printf("%d ",play[x][y]);	//remember to change special char to reflect any new variables
+   	play[x][y]= deckmaster[drawcard()];
+   	printf("%d ",play[x][y]);	//remember to change format char to reflect any new variables
    	++m;						//m is initalised at 0 at top of code
    }
 
     printf("\n");
-
   }
 
   return 0;
